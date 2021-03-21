a835b6a1489ad17d5ac8dbbcb1a935148f7bdecc
ndsarno <ndsarno16@gmail.com>
2021-03-21 11:24:08 -0500

    Initial Commit

---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinuedLearningApp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Nick Sarno on 3/21/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
